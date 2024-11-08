f0e26e8d45c3cb5845367ef3c21a185ff1536f0c
Aleksei Denisov <tech.denisov@gmail.com>
2024-11-08 21:18:53 +0400

    feat: add isString function implementation

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -40,8 +40,12 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  // eslint-disable-next-line eqeqeq
+  if (value != undefined || value != null) {
+    return typeof value.valueOf() === 'string';
+  }
+  return false;
 }
 
 /**
