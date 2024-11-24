e74ec1a086caca99d9beb7a27f169217144fc4bb
Aleksei Denisov <tech.denisov@gmail.com>
2024-11-24 15:45:16 +0400

    feat: add isPalindrome function implementation

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -355,8 +355,11 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  return (
+    str.toUpperCase().replaceAll(/\W/g, '') ===
+    reverseString(str).toUpperCase().replaceAll(/\W/g, '')
+  );
 }
 
 /**
