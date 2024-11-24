03cd2c433dcbd72e2f03bb9ea6448ac651189857
Aleksei Denisov <tech.denisov@gmail.com>
2024-11-24 19:15:48 +0400

    feat: add reverseWords function implementation

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -397,8 +397,19 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const words = str.split(' ');
+  let sentence = '';
+
+  for (let i = 0; i < words.length; i += 1) {
+    sentence += reverseString(words[i]);
+
+    if ((words.length !== 1 && i === 0) || i !== words.length - 1) {
+      sentence += ' ';
+    }
+  }
+
+  return sentence;
 }
 
 /**
